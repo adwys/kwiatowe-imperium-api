d92f7f3281655cf26a5b113ed6ae2924b8607d72
Adam Wysocki <adwyspl@gmail.com>
2022-03-04 15:47:37 +0100

    setup project

---
+++ src/main/java/com/kwiatowe_imperium/api/KwiaciarniaApplication.java
@@ -0,0 +1,13 @@
+package com.kwiatowe_imperium.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwiaciarniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwiaciarniaApplication.class, args);
+	}
+
+}
